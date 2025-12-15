d149d5400023cad7fbe8339e9be540b2b73a469c
GeronimoAhil <geromy2014@gmail.com>
2025-12-15 21:18:08 +0800

    Add files via upload

---
+++ Shutdown.py
@@ -0,0 +1,9 @@
+def yes():
+    if (n=='yes'):
+        print("The computer is exiting out. 3,2,1, buzzz!")
+    else:
+        print("The shutdown is aborted. Please continue to work...")
+    return
+
+n = input("Would you want to exit out this device: ")
+yes()
